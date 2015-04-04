5d5857605459c4964addbd687415961553dca115
Kaiser <Kaiserddekam@gmail.com>
2015-04-04 12:56:55 -0400

    UI Change to Video page
    -Realigned social media icons
    -Twitter Link now works
    -Created a new UI for the video page and removed all javascript to
    start over with the new UI.
    -NO javascript is functional.

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Page Loaded")  
+
+document.getElementById("video-editing").onclick = function () {
+    document.getElementById("pt-promo").style.display="block";
+    console.log("Video CLicked")
+}
+var index = 0;
+document.getElementById("video-editing").onclick = function () {
+
+
